2f54eb2ce2042fc4edfe7dffa0535fd88923e500
jadontsai <154614626+jadontsai@users.noreply.github.com>
2026-03-29 20:51:11 -0400

    Add initial test for hall effect sensor
    This module implements an initial test for the hall effect sensor.

--- sensor_code/hall_effect/initial_hall_effect_test.py
+++ sensor_code/hall_effect/initial_hall_effect_test.py
@@ -1,3 +1,12 @@
+"""
+File: initial_hall_effect_test.py
+
+Description:
+This module implements an initial test for the hall effect sensor.
+
+Author: Jadon Tsai
+Date: 2026-03-21
+"""
 import time
 import board
 import digitalio
@@ -7,7 +16,6 @@ hall.direction = digitalio.Direction.INPUT
 
 last_state = hall.value
 
-print("AH372 ready")
 print("OUT =", "HIGH" if last_state else "LOW")
 
 while True:

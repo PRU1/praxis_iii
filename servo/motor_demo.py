7c5d26aef38ba59d4e53d4f30f65d0a1cef088c9
jadontsai <154614626+jadontsai@users.noreply.github.com>
2026-03-29 20:46:57 -0400

    Create motor_demo.py for testing multiple motors
    This module tests the functionality of multiple motors at once. This is an initial working test, so no specifications are tested.

--- servo/motor_demo.py
+++ servo/motor_demo.py
@@ -1,8 +1,19 @@
+"""
+File: motor_demo.py
+
+Description:
+This module tests the functionality of multiple motors at once.
+This is an initial working test, so no specifications are tested.
+
+Author: Jadon Tsai
+Date: 2026-03-26
+"""
 import time
 import board
 import pwmio
 from adafruit_motor import servo
 
+# declaring variables
 pwm1 = pwmio.PWMOut(board.GP0, duty_cycle=2**15, frequency=50)
 pwm2 = pwmio.PWMOut(board.GP1, duty_cycle=2**15, frequency=50)
 pwm3 = pwmio.PWMOut(board.GP2, duty_cycle=2**15, frequency=50)
@@ -11,10 +22,13 @@ servo_motor1 = servo.Servo(pwm1)
 servo_motor2 = servo.Servo(pwm2)
 servo_motor3 = servo.Servo(pwm3)
 
-#180 is full closed, turns ccw from 0 to 180
-#110 is full open
-#while True:
+#180 is full closed angle, turns ccw from 0 to 180
+#110 is full open angle
 def calibration():
+    """
+    Sets all the motor angles to a known angle, so that we can then 
+    attach the servo arms
+    """
     servo_motor1.angle = 180
     time.sleep(2)
     servo_motor2.angle = 180
@@ -26,7 +40,11 @@ def testing():
     servo_motor2.angle = 180
 
 def demo():
-
+    """
+    Moving servos simultaneously from
+    full open position (110 degrees)
+    to full closed (180 degrees)
+    """
     time.sleep(1)
     servo_motor1.angle = 110
     servo_motor2.angle = 110
